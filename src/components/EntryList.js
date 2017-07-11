b1eb650c63ac95a39a311a40bff900ecc6a094fa
jeffdiers <jeff.diers@gmail.com>
2017-07-11 16:22:51 -0600

    routes setup

---
+++ src/components/EntryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EntryList extends Component{
+    render() {
+        return (
+            <div>
+                I am the EntryList!
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { render } from 'react-dom'
+
+// Import css
+import './index.css'
+
+// Import Components
+import Main from './components/Main'
+import Single from './components/Single'
+import EntryList from './components/EntryList'
+
+// Import react router dependecies
+import { Router, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+
+const router = (
+    <BrowserRouter>
+        <div>
+            <Main/>
+            <Route exact path='/' component={EntryList}></Route>
+            <Route path='/view' component={Single}></Route>
+        </div>
+    </BrowserRouter>
+)
+
+render(router, document.getElementById('root'))
